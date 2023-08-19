fdf2627ff20f1959a3bb78fdee807efe13793e89
derrickkim0109 <derrickkim0109@naver.com>
2023-08-19 16:28:56 +0900

    refactor: 불필요한 부분 제거

--- Projects/Modules/CoreKit/Sources/Colors.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import SwiftUI
-
-public typealias SquareColors = SharedDesignSystemAsset.Colors
-
-public extension Color {
-}
--- Projects/Modules/CoreKit/Sources/Images.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public typealias SquareImages = SharedDesignSystemAsset.Images
-
-import SwiftUI
-
-public extension Image {
-    static let pizza = SharedDesignSystemAsset.Images.pizza.swiftUIImage
-}
